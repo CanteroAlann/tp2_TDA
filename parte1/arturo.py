fdd5b8485b2173f5671aea94da83c4a37450bb46
martinbucca <mbucca@fi.uba.ar>
2023-10-26 09:36:46 -0300

    [refactor] paso seguimiento de solucion loacl y global a constantes

--- parte1/arturo.py
+++ parte1/arturo.py
@@ -6,35 +6,34 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 
-
 def maxima_habilidad_caballero(caballeros):
     if len(caballeros) == 0:
         return []
     caballeros_circular = caballeros + caballeros    
-    optimos = [0 for _ in range(len(caballeros_circular))]
-    optimos[0] = caballeros[0][1]
-    max_habilidad = optimos[0]
-    caballeros_elegidos_local = [caballeros[0][0]]
-    caballeros_elegidos_global = [caballeros[0][0]]
+    max_global = caballeros_circular[0][1]
+    max_local = caballeros_circular[0][1]
+    start = 0
+    end = 0
     c = 1
     for i in range(1, len(caballeros_circular)):
-        if optimos[i-1] + caballeros_circular[i][1] > caballeros_circular[i][1]:
-            optimos[i] = optimos[i-1] + caballeros_circular[i][1]
-            caballeros_elegidos_local.append(caballeros_circular[i][0])            
-            if optimos[i] > max_habilidad:
-                caballeros_elegidos_global = caballeros_elegidos_local[:]
-                max_habilidad = optimos[i]
+        if max_local + caballeros_circular[i][1] > caballeros_circular[i][1]:
+            max_local = max_local + caballeros_circular[i][1]
+            if max_local > max_global:
+                max_global = max_local
+                end = i
             c += 1
         else:
-            optimos[i] = caballeros_circular[i][1]
-            caballeros_elegidos_local = [caballeros_circular[i][0]]
-            if optimos[i] > max_habilidad:
-                caballeros_elegidos_global = [caballeros_circular[i][0]]
-                max_habilidad = optimos[i]
+            max_local = caballeros_circular[i][1]
+            if max_local > max_global:
+                max_global = max_local
+                start = i
+                end = i
             c = 1
-        if c == caballeros_elegidos_local:
+        if c == len(caballeros):
             break
-    return caballeros_elegidos_global
+    return [caballero[0] for caballero in caballeros_circular[start:end+1]]
+
+
 
 
 
