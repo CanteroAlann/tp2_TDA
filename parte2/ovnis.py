4fc5449765f050fd2e658a736ba7d159e5b0d80f
martinbucca <mbucca@fi.uba.ar>
2023-10-29 18:28:08 -0300

    prints de conexiones a mejorar

--- parte2/ovnis.py
+++ parte2/ovnis.py
@@ -9,6 +9,7 @@ if len(sys.argv) != 2:
 
 def obtener_flujo_maximo(red, fuente, sumidero):
     flujo = {}
+    # inicializo flujo entre todos los vertices = 0
     for v in red.obtener_vertices():
         for w in red.adyacentes(v):
             flujo[(v, w)] = 0
@@ -25,25 +26,24 @@ def obtener_flujo_maximo(red, fuente, sumidero):
                 actualizar_grafo_residual(grafo_residual, camino[i], camino[i-1], int(bottleneck))
 
         camino = obtener_camino(grafo_residual, fuente, sumidero)
-    
-
-
-
     return flujo, grafo_residual
-
-
 
     
 
 def actualizar_grafo_residual(grafo_residual, v, w, bottleneck):
+    '''Dados un grafo residual, dos vertices y un bottleneck, actualiza el grafo residual'''
     peso_anterior = grafo_residual.peso_arista(v, w)
     if peso_anterior == bottleneck:
+        # ya fluye la capacidad maxima por esa arista, la elimino
         grafo_residual.borrar_arista(v, w)
     else:
+        # actualizo la capacidad residual de la arista
         grafo_residual.cambiar_peso_arista(v, w, peso_anterior - bottleneck)
     if not grafo_residual.son_adyacentes(w, v):
+        # si no existe la arista inversa, la creo con flujo = bottleneck
         grafo_residual.agregar_arista(w, v, bottleneck)
     else:
+        # si existe, actualizo el flujo que pasa en esa arista
         grafo_residual.cambiar_peso_arista(w, v, peso_anterior + bottleneck)
 
 
@@ -69,7 +69,7 @@ def obtener_camino(red, fuente, sumidero):
 
             # Si la capacidad residual es mayor que cero, significa que hay un camino
             # válido en la red residual
-            if capacidad_residual > 0:
+            if capacidad_residual and capacidad_residual > 0:
                 # Agregar el vecino al camino actual
                 nuevo_camino = camino_actual + [vecino]
 
@@ -103,8 +103,7 @@ def corte_minimo(red, grafo_residual):
     for nodo in nodos_alcanzables:
         for adyacente in red.adyacentes(nodo):
             if adyacente not in nodos_alcanzables:
-                corte.add((nodo, adyacente))
-
+                corte.add((nodo, red.peso_arista(nodo, adyacente),adyacente))
     return corte
     
 def min_peso(red, camino):
@@ -121,12 +120,19 @@ def mostrar_configuracion_conexiones(flujo):
         if flujo[conexion] > 0:
             print(f"{conexion[0]}  -- {flujo[conexion]} -->  {conexion[1]}")
 
+def mostar_corte_minimo(corte_minimo):
+    '''Muestra el corte minimo'''
+    print("Conexiones a mejorar para aumentar capacidad de transmisión de información de la red:")
+    for conexion in corte_minimo:
+        print(f"{conexion[0]}  -- {conexion[1]} -->  {conexion[2]}")
+
 def main():
     archivo_red_servidores = sys.argv[1]
     red = file_reader.leer_archivo(archivo_red_servidores)
     flujo, grafo_residual = obtener_flujo_maximo(red, 'R', 'S')
+    corte = corte_minimo(red, grafo_residual)
     mostrar_configuracion_conexiones(flujo)
-    print(corte_minimo(red, grafo_residual))
+    mostar_corte_minimo(corte)
 
 if __name__ == "__main__":
     main()
